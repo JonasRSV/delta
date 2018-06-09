c9490267f25bdd29a9448ed162126e635448befc
jonas@valfridsson.net <jonas@valfridsson.net>
2018-06-09 21:20:04 +0200

    Update DB and make sure motion / document requests work with docker

--- delta/content.py
+++ delta/content.py
@@ -3,13 +3,46 @@ import delta.database
 from delta.config import config
 import json
 import sys
+import datetime
 
 DB = delta.database.Connection(config)
 DB.connect()
 
+class Generic(object):
+    def parse_response(cell):
+        return {  "id"     : cell[0]
+               , "date"    : str(cell[1])
+               , "title"   : cell[2]
+               , "document": json.loads(str(cell[3]))
+               , "type"    : cell[4]
+               }
+
+class Motion(object):
+    identifier = "mot"
+
+    def parse_response(cell):
+        """
+        0: ID from Document
+        1: Date from Document
+        2: Title from Doucument
+        3: Actual Document
+        4: Type from Document
+        5: ID from Motion
+        6: Summary from Motion
+        7: Party Affiliation Motion
+        """
+        return {  "id"     : cell[0]
+               , "date"    : str(cell[1])
+               , "title"   : cell[2]
+               , "document": json.loads(str(cell[3]))
+               , "type"    : cell[4]
+               , "summary" : cell[6]
+               , "party"   : cell[7]
+               }
 
 class Document(object):
     """Document request object."""
+    identifier = "document"
 
     def __init__(self, id, type, date, limit):
         self.id    = id
@@ -21,6 +54,8 @@ class Document(object):
         self.sz = None
         self.json = None
 
+        self.handlers = { Motion.identifier: Motion.parse_response }
+
     def set_payload(self, payload):
         self.payload = payload
         self.sz = len(payload)
@@ -28,12 +63,10 @@ class Document(object):
         return self
 
     def parse_response(self, cell):
-        return {  "id":       cell[0]
-                , "date":     str(cell[1])
-                , "title":    cell[2]
-                , "document": cell[3]
-               }
-
+        if self.type in self.handlers:
+            return self.handlers[self.type](cell)
+        else:
+            return Generic.parse_response(cell)
 
     def jsonify(self):
         try:
@@ -46,8 +79,19 @@ class Document(object):
 
         return self.json
 
+class Request(object):
 
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
 
+    def set_type(self, type):
+        self.type = type
+        return self
+
+    def set_data(self, data):
+        self.data = data
+        return self
 
 content = flask.Blueprint("Content handler", __name__)
 
@@ -60,8 +104,14 @@ def get_documents():
     date  = flask.request.args.get("date")
     limit = flask.request.args.get("limit")
 
-    request  = Document(id, type, date, limit)
-    response = DB.request_documents(request)
+    if date is None:
+        date = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    if type is None:
+        type = Document.identifier
+
+    request  = Request(type, Document(id, type, date, limit))
+    response = DB.request(request)
 
     return flask.Response(response.jsonify(), status=200)
 
--- delta/database.py
+++ delta/database.py
@@ -2,15 +2,94 @@ import psycopg2
 import sys
 
 
+class Motion(object):
+
+    ID      =   "SELECT * FROM DOCUMENT JOIN MOTION ON "\
+            +   "DOCUMENT.ID = MOTION.ID WHERE %s LIKE DOCUMENT.ID "\
+            +   "AND TIME > %s LIMIT %s;"
+
+    TYPE    =   "SELECT * FROM DOCUMENT JOIN MOTION ON "\
+            +   "DOCUMENT.ID = MOTION.ID WHERE %s LIKE DOCUMENT.TYPE "\
+            +   "AND TIME > %s LIMIT %s;"
+
+    REGULAR =   "SELECT * FROM DOCUMENT JOIN MOTION ON "\
+            +   "DOCUMENT.ID = MOTION.ID AND TIME > %s LIMIT %s;"
+
+    def request(cursor, request):
+        limit = request.limit
+        if limit is None:
+            limit = 10000
+
+        try:
+            if request.id is not None:
+                cursor.execute( Motion.ID
+                              , (request.id, request.date, limit))
+
+                return request.set_payload(cursor.fetchall())
+            elif request.type is not None:
+                cursor.execute( Motion.TYPE
+                              , (request.type, request.date, limit))
+
+                return request.set_payload(cursor.fetchall())
+            else:
+                cursor.execute( Motion.REGULAR
+                              , (request.date, limit))
+
+                return request.set_payload(self.cursor.fetchall())
+
+        except Exception as e:
+            print(e)
+            """Add error handling."""
+            print("Failed to get requested document: {}".format(str(e)))
+            sys.exit(1)
+
+        return request
+
+
+
+class Document(object):
+    ID      =   "SELECT * FROM DOCUMENT WHERE %s LIKE ID AND "\
+            +   "TIME > %s LIMIT %s;"
+
+    TYPE    =   "SELECT * FROM DOCUMENT where %s LIKE TYPE AND "\
+            +   "TIME > %s LIMIT %s;"
+
+    REGULAR =   "SELECT * FROM DOCUMENT WHERE TIME > %s LIMIT %s;"
+
+    def request(cursor, request):
+        limit = request.limit
+        if limit is None:
+            limit = 10000
+
+        try:
+            if request.id is not None:
+                cursor.execute( Document.ID
+                              , (request.id, request.date, limit))
+
+                return request.set_payload(cursor.fetchall())
+            elif request.type is not None:
+                cursor.execute( Document.TYPE
+                              , (request.type, request.date, limit))
+
+                return request.set_payload(cursor.fetchall())
+            else:
+                cursor.execute( Document.REGULAR
+                              , (request.date, limit))
+
+                return request.set_payload(self.cursor.fetchall())
+
+        except Exception as e:
+            print(e)
+            """Add error handling."""
+            print("Failed to get requested document: {}".format(str(e)))
+            sys.exit(1)
+
+        return request
+
+
 class Connection(object):
     """DB Connection handler."""
     
-    DOC_ID_OR_TYPE =   "SELECT * FROM DOCUMENTS WHERE %s LIKE ID AND "\
-                     + "TIME > %s LIMIT %s;"
-
-    DOC_REGULAR    =   "SELECT * FROM DOCUMENTS WHERE TIME > %s LIMIT %s;"
-
-
     def __init__(self, config):
         self.config = config
         self.connection = None
@@ -21,7 +100,10 @@ class Connection(object):
                    , config["database"]["user"]
                    , config["database"]["password"])
 
-        self.cursor = None
+        self.cursor   = None
+
+        self.handlers = { "document": Document.request
+                        , "mot"  : Motion.request}
 
     def connect(self):
         """Connect to database."""
@@ -49,38 +131,16 @@ class Connection(object):
 
         return None
 
-    def request_documents(self, doc_request):
+    def request(self, request):
         """Get documents according to doc_request specification."""
         self.check_connection()
 
-        limit = doc_request.limit
-        if limit is None:
-            limit = 10000
+        if request.type in self.handlers:
+            return self.handlers[request.type](self.cursor, request.data)
+        else:
+            raise NotImplementedError("Don't regonize request {}".format(self.type))
 
-        try:
-            if doc_request.id is None and doc_request.type is None:
-                self.cursor.execute(  Connection.DOC_REGULAR
-                                   , (doc_request.date, limit))
-
-                return doc_request.set_payload(self.cursor.fetchall())
-
-            if not doc_request.id is None:
-                self.cursor.execute(  Connection.DOC_ID_OR_TYPE
-                                   ,  (doc_request.id, doc_request.date, limit))
-
-                return doc_request.set_payload(self.cursor.fetchall())
-
-            self.cursor.execute(  Connection.DOC_ID_OR_TYPE
-                               ,  (doc_request.type, doc_request.date, limit))
-
-            return doc_request.set_payload(self.cursor.fetchall())
-        except Exception as e:
-            print(e)
-            """Add error handling."""
-            print("Failed to get requested document: {}".format(str(e)))
-            sys.exit(1)
-
-        return None
+        return request
 
 
 
