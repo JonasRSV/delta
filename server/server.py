7b732741e1a4d2539d446816109e18e3fa5eef5f
jonas@valfridsson.net <jonas@valfridsson.net>
2018-06-30 14:39:43 +0200

    Stuff

--- server/server.py
+++ server/server.py
@@ -3,11 +3,17 @@ from delta.blueprints.content  import content
 from delta.blueprints.logs     import logs
 from delta.blueprints.users    import users
 
+#REMOVE LATER
+from flask_cors import CORS
+
 app = flask.Flask(__name__)
 
 app.register_blueprint(content)
 app.register_blueprint(logs)
 app.register_blueprint(users)
+
+#REMOVE LATER
+CORS(app)
 
 
 @app.route("/")
@@ -16,5 +22,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=4999)
 
